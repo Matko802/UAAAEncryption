3ff71cdf11e29928e909acdbc50c1da8ff3feee9
Matko802 <149497907+Matko802@users.noreply.github.com>
2026-07-16 21:22:47 +0200

    Bump version to 0.0.2

--- UAAADecryptor.user.js
+++ UAAADecryptor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UAAADecryptor
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  UAAAEncryption custom encryptor/decryptor
 // @author       Matko802
 // @match        *://*/*
