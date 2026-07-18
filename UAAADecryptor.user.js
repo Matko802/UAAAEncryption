df594fdf89007423f5f88baeb08c65347170f8cf
Matko802 <matkohrad@gmail.com>
2026-07-18 14:27:12 +0200

    Bump version to 0.0.9 in UAAADecryptor.user.js

--- UAAADecryptor.user.js
+++ UAAADecryptor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UAAADecryptor
 // @namespace    http://tampermonkey.net/
-// @version      0.0.8
+// @version      0.0.9
 // @description  UAAAEncryption custom encryptor/decryptor
 // @author       Matko802
 // @match        *://*/*
