282f1155b25551fb427c26b5c667f55cb6272d34
Matko802 <matkohrad@gmail.com>
2026-07-18 18:27:10 +0200

    dwdadw

--- UAAADecryptor.user.js
+++ UAAADecryptor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UAAADecryptor
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  UAAAEncryption custom encryptor/decryptor
 // @author       Matko802
 // @match        *://*/*
